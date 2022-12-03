003a469def4c12a7b63720a9e353d141c7b6a8a4
Justin Reardon <justin.reardon@gmail.com>
2022-12-03 14:12:02 -0500

    Simplify day 3

--- src/main/scala/com/jmreardon/aoc2022/Day03.scala
+++ src/main/scala/com/jmreardon/aoc2022/Day03.scala
@@ -6,29 +6,21 @@ import cats.effect._
 import fs2.io.file._
 import scala.collection.mutable.PriorityQueue
 import scala.collection.immutable.SortedSet
+import cats.Functor
+import cats.Traverse
 
 object Day03 extends Day(3):
   override def a =
     readFile
-      .map(line =>
-        line.splitAt(line.length / 2) match {
-          case (a, b) =>
-            extractContents(a).intersect(extractContents(b)).toList.map(priority).sum
-        }
-      )
+      .map(line => contentPriority(line.splitAt(line.length / 2).toList))
       .fold(0)(_ + _)
       .map(_.toString)
 
-  override def b =
-    readFile
-      .map(extractContents)
-      .chunkN(3, false)
-      .map(_.reduceLeftOption(_ intersect _).getOrElse(Set.empty).map(priority).sum)
-      .fold(0)(_ + _)
-      .map(_.toString)
+  override def b = readFile.chunkN(3, false).map(contentPriority).fold(0)(_ + _).map(_.toString)
 
   private val readFile = Files[IO].readUtf8Lines(Path(s"data/day03/input.txt")).filter(_.nonEmpty)
 
-  private def extractContents(bagContents: String) = bagContents.toSet
+  private def contentPriority[F[_]: Traverse](contents: F[String]) =
+    contents.map(_.toSet).reduceLeftOption(_ intersect _).getOrElse(Set.empty).map(priority).sum
 
   private def priority(c: Char): Int = 1 + c - (if c >= 'a' then 'a' else ('A' - 26))
